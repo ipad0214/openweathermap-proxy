b2b43b8db48abc104fbe2cc76de6c5d5817658a7
patrick kuckelsberg <kuckelsberg.patrick@gnmail.com>
2019-10-16 09:17:34 +0200

    added .gitignore and deleted unnecessary files

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = require("./main");
-const PORT = 5001;
-main_1.default.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
-});
-//# sourceMappingURL=server.js.map
